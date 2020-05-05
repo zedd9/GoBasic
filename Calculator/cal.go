b392fe621d6c705659e0f9c8cfd85191d33fdc35
Zedd <gusdnre@gmail.com>
2020-05-05 16:42:31 +0900

    [기초] 이항 연산자

---
+++ Calculator/cal.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("숫자를 입력하세요")
+	reader := bufio.NewReader(os.Stdin)
+
+	line, _ := reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+	n1, _ := strconv.Atoi(line)
+
+	line, _ = reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+	n2, _ := strconv.Atoi(line)
+
+	fmt.Printf("입력하신 숫자는 %d, %d 입니다.\n", n1, n2)
+
+	fmt.Println("연산자를 입력하세요.")
+	line, _ = reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+
+	if line == "+" {
+		fmt.Printf("%d + %d = %d", n1, n2, n1+n2)
+	} else if line == "-" {
+		fmt.Printf("%d - %d = %d", n1, n2, n1-n2)
+	} else if line == "*" {
+		fmt.Printf("%d * %d = %d", n1, n2, n1*n2)
+	} else if line == "/" {
+		fmt.Printf("%d / %d = %d", n1, n2, n1/n2)
+	} else {
+		fmt.Println("잘못 입력하셨습니다.")
+	}
+}
