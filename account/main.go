c63f1a755c74d9d8ca4bda384ae63ab52ace10b3
Zedd <gusdnre@gmail.com>
2020-05-05 23:36:02 +0900

    [Thread] Acccount 클래스 활용 - Mutex 사용

---
+++ account/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type Account struct {
+	balance int
+	mutex   *sync.Mutex
+}
+
+func (a *Account) Widthdraw(val int) {
+	a.mutex.Lock()
+	a.balance -= val
+	a.mutex.Unlock()
+}
+
+func (a *Account) Deposit(val int) {
+	a.mutex.Lock()
+	a.balance += val
+	a.mutex.Unlock()
+}
+
+func (a *Account) Balance() int {
+	a.mutex.Lock()
+	balance := a.balance
+	a.mutex.Unlock()
+	return balance
+}
+
+var accounts []*Account
+var globalLock *sync.Mutex
+
+func Transfer(sender, receiver int, money int) {
+	globalLock.Lock()
+	accounts[sender].Widthdraw(money)
+	accounts[receiver].Deposit(money)
+	globalLock.Unlock()
+}
+
+func GetTotalBalance() int {
+	total := 0
+
+	globalLock.Lock()
+	for i := 0; i < len(accounts); i++ {
+		total += accounts[i].Balance()
+	}
+	globalLock.Unlock()
+
+	return total
+}
+
+func RandomTransfer() {
+	var sender, balance int
+	for {
+		sender = rand.Intn(len(accounts))
+		balance = accounts[sender].Balance()
+		if balance > 0 {
+			break
+		}
+	}
+
+	var receiver int
+	for {
+		receiver = rand.Intn(len(accounts))
+		if sender != receiver {
+			break
+		}
+	}
+
+	money := rand.Intn(balance)
+	Transfer(sender, receiver, money)
+}
+
+func GoTransfer() {
+	for {
+		RandomTransfer()
+	}
+}
+
+func PrintTotalBalance() {
+	fmt.Printf("Total: %d\n", GetTotalBalance())
+}
+
+func main() {
+	globalLock = &sync.Mutex{}
+	for i := 0; i < 20; i++ {
+		accounts = append(accounts, &Account{balance: 10000, mutex: &sync.Mutex{}})
+	}
+
+	PrintTotalBalance()
+
+	for i := 0; i < 10; i++ {
+		go GoTransfer()
+	}
+
+	for {
+		PrintTotalBalance()
+		time.Sleep(100 * time.Millisecond)
+	}
+}
