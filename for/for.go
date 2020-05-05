cf2b47e0787dfc12a421cd8a82241b3f5e1bf912
Zedd <gusdnre@gmail.com>
2020-05-05 17:12:48 +0900

    [기초] for문(3) - * 출력하기

--- for/for.go
+++ for/for.go
@@ -3,11 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	for i := 2; i <= 9; i++ {
-		fmt.Printf("%d단\n", i)
+	for i := 0; i < 5; i++ {
+		for j := 0; j < i+1; j++ {
+			fmt.Printf("*")
+		}
 
-		for j := 1; j <= 9; j++ {
-			fmt.Printf("%d * %d = %d\n", i, j, i*j)
+		fmt.Println()
+	}
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5-i; j++ {
+			fmt.Printf("*")
+		}
+
+		fmt.Println()
+	}
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 3-i; j++ {
+			fmt.Printf(" ")
+		}
+
+		for j := 0; j < 2*i+1; j++ {
+			fmt.Printf("*")
+		}
+
+		fmt.Println()
+	}
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < i+1; j++ {
+			fmt.Printf(" ")
+		}
+
+		for j := 0; j < 5-2*i; j++ {
+			fmt.Printf("*")
 		}
 
 		fmt.Println()
