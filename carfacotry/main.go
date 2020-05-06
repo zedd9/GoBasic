cd6362cca9f3c74122f30c5fe82dd1d9f3d86b1e
Zedd <gusdnre@gmail.com>
2020-05-07 01:26:18 +0900

    [Select] select문 활용 - time.Tick - time.After

--- carfacotry/main.go
+++ carfacotry/main.go
@@ -10,25 +10,37 @@ type Car struct {
 	val string
 }
 
-func MakeTire(carChan chan Car, outChan chan Car) {
-	for {
-		car := <-carChan
-		car.val += "Tire, "
+type Plane struct {
+	val string
+}
 
-		outChan <- car
+func MakeTire(carChan chan Car, planeChan chan Plane, outCarChan chan Car, outPlaneChan chan Plane) {
+	for {
+		select {
+		case car := <-carChan:
+			car.val += "Tire_C, "
+			outCarChan <- car
+		case plane := <-planeChan:
+			plane.val += "Tire_P, "
+			outPlaneChan <- plane
+		}
 	}
 }
 
-func MakeEngine(carChan chan Car, outChan chan Car) {
+func MakeEngine(carChan chan Car, planeChan chan Plane, outCarChan chan Car, outPlaneChan chan Plane) {
 	for {
-		car := <-carChan
-		car.val += "Engine, "
-
-		outChan <- car
+		select {
+		case car := <-carChan:
+			car.val += "Engine_C, "
+			outCarChan <- car
+		case plane := <-planeChan:
+			plane.val += "Engine_P, "
+			outPlaneChan <- plane
+		}
 	}
 }
 
-func StartWork(chan1 chan Car) {
+func StartCarWork(chan1 chan Car) {
 	i := 0
 
 	for {
@@ -38,17 +50,36 @@ func StartWork(chan1 chan Car) {
 	}
 }
 
-func main() {
-	chan1 := make(chan Car)
-	chan2 := make(chan Car)
-	chan3 := make(chan Car)
-
-	go StartWork(chan1)
-	go MakeTire(chan1, chan2)
-	go MakeEngine(chan2, chan3)
+func StartPlaneWork(chan1 chan Plane) {
+	i := 0
 
 	for {
-		result := <-chan3
-		fmt.Println(result.val)
+		time.Sleep(1 * time.Second)
+		chan1 <- Plane{val: "plane" + strconv.Itoa(i)}
+		i++
+	}
+}
+
+func main() {
+	carChan1 := make(chan Car)
+	carChan2 := make(chan Car)
+	carChan3 := make(chan Car)
+
+	planeChan1 := make(chan Plane)
+	planeChan2 := make(chan Plane)
+	planeChan3 := make(chan Plane)
+
+	go StartCarWork(carChan1)
+	go StartPlaneWork(planeChan1)
+	go MakeTire(carChan1, planeChan1, carChan2, planeChan2)
+	go MakeEngine(carChan2, planeChan2, carChan3, planeChan3)
+
+	for {
+		select {
+		case result := <-carChan3:
+			fmt.Println(result.val)
+		case result := <-planeChan3:
+			fmt.Println(result.val)
+		}
 	}
 }
